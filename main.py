9676799a4db2f96fdf6079db5e162e1aa6e790a0
Liam Tangney <ltags230@gmail.com>
2024-04-21 19:44:38 -0500

    updated app modes

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
-import argparse as ap
+import argparse
+import configparser
 
 from src.app import AppController
+import resources
 
 
-def parse_args() -> ap.Namespace:
-    parser = ap.ArgumentParser(
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
         prog='eTRM Measure Summary Tool',
         description='Creates a summary of one or more measures.')
 
-    parser.add_argument('-t', '--token',
-        metavar='token',
+    parser.add_argument('-m', '--mode',
+        metavar='mode',
         type=str,
-        help='eTRM authorization token (API key)')
+        choices=['client', 'dev'],
+        default='client',
+        help='specify which mode to run the app in (default: client)')
 
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
-    token: str | None = getattr(args, 'token', None)
-    if token != None:
-        token = 'Token ' + token
-    main_app = AppController()
-    main_app.run(auth_token=token)
+    mode: str = getattr(args, 'mode', 'client')
+    controller = AppController()
+    if mode == 'dev':
+        config = configparser.ConfigParser()
+        config.read(resources.get_path('config.ini'))
+        controller.connect(f'{config["etrm"]["type"]} {config["etrm"]["token"]}')
+    controller.run()
 
 
 if __name__ == '__main__':
