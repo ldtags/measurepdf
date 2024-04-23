a0a590980871eff13a5e84731a51fd47a901e135
Liam Tangney <ltags230@gmail.com>
2024-04-23 14:16:43 -0500

    better resources

--- src/resources/__init__.py
+++ src/resources/__init__.py
@@ -1,9 +1,9 @@
 import os
 
-_ROOT = os.path.abspath(os.path.dirname(__file__))
+_PATH = os.path.abspath(os.path.dirname(__file__))
 
 def get_path(file_name: str) -> str:
-    _path = os.path.join(_ROOT, file_name)
-    if not os.path.exists(_path):
+    file_path = os.path.join(_PATH, file_name)
+    if not os.path.exists(file_path):
         raise FileNotFoundError(f'no resource named {file_name} exists')
-    return _path
+    return file_path
