ed97e78963b4be731c85a7e412c32987c976e9c8
Liam Tangney <ltags230@gmail.com>
2024-04-23 14:13:37 -0500

    new tests

--- tests/context/src.py
+++ tests/context/src.py
@@ -5,3 +5,4 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..',
 import src.etrm as etrm
 import src.measurepdf as measurepdf
 import src.app as app
+import src.main as main
