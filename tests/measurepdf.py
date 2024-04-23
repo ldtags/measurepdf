5d54b31c10894848c34a830c47384125b5226860
Liam Tangney <ltags230@gmail.com>
2024-04-23 15:40:48 -0500

    updated pdf tests

--- tests/measurepdf.py
+++ tests/measurepdf.py
@@ -1,15 +1,28 @@
+import sys
 from configparser import ConfigParser
 
 from context import resources, etrm, measurepdf
 
 
-def test():
-    config = ConfigParser()
-    config.read(resources.get_path('config.ini'))
-    connection = etrm.ETRMConnection(config['etrm']['type'] + ' ' + config['etrm']['token'])
+config = ConfigParser()
+config.read(resources.get_path('config.ini'))
+connection = etrm.ETRMConnection(config['etrm']['type'] + ' ' + config['etrm']['token'])
+
+
+def build_summary(measure_ids: list[str]):
     measure_pdf = measurepdf.MeasurePdf(relative_dir='summaries', override=True)
-    measure_pdf.add_measure(connection.get_measure('SWAP001-03'))
+    print('measure pdf object created', file=sys.stderr)
+
+    for measure_id in measure_ids:
+        measure_pdf.add_measure(connection.get_measure(measure_id))
+        print(f'added measure {measure_id}', file=sys.stderr)
+
     measure_pdf.build()
+    print(f'measure summary {measure_pdf.file_name} was successfully created')
+
+
+def test():
+    build_summary(['SWAP001-03'])
 
 
 if __name__ == '__main__':
