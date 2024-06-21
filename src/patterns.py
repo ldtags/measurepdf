eeda21b2ae930b5dcec7a626c7937683f78f1c15
Liam Tangney <ltags230@gmail.com>
2024-06-21 10:18:24 -0500

    added use category pattern

--- src/patterns.py
+++ src/patterns.py
@@ -1,4 +1,15 @@
+"""Compiled general use RegEx patterns"""
+
+
 import re
+
+
+__UC_ID = r'^SW([A-Za-z]{2})$'
+UC_ID = re.compile(__UC_ID)
+"""Statewide Use Category ID RegEx Pattern
+
+Group 1: Use Category
+"""
 
 
 __STWD_ID = r'^[A-Za-z]{3,}[0-9]{2,}$'
