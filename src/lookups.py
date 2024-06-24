500d873104980f0031cddf6c7ca6419d0f64f912
Liam <ltags230@gmail.com>
2024-06-24 13:24:42 -0500

    added use category lookups

---
+++ src/lookups.py
@@ -0,0 +1,16 @@
+USE_CATEGORIES = {
+    'AP': 'Appliance or Plug Load',
+    'BE': 'Building Envelope',
+    'CA': 'Compressed Air',
+    'CR': 'Commercial Refrigeration',
+    'FS': 'Food Service',
+    'HC': 'HVAC',
+    'LG': 'Lighting',
+    'MI': 'Miscellanious',
+    'PR': 'Process',
+    'RE': 'Recreation',
+    'SV': 'Service',
+    'WB': 'Whole Building',
+    'WH': 'Service & Domestic Hot Water',
+    'WP': 'Water Pumping / Irrigation'
+}
