eb06a688d910cbf166a5d68f563a83e24c3e390e
Liam <ltags230@gmail.com>
2024-06-24 15:55:05 -0500

    added use category lookups

--- src/lookups.py
+++ src/lookups.py
@@ -14,3 +14,21 @@ USE_CATEGORIES = {
     'WH': 'Service & Domestic Hot Water',
     'WP': 'Water Pumping / Irrigation'
 }
+
+
+PERMUTATION_REFS = {
+    'SWAP': 'R3200',
+    'SWBE': 'R3201',
+    'SWCA': 'R3202',
+    'SWCR': 'R3203',
+    'SWFS': 'R3204',
+    'SWHC': 'R3205',
+    'SWLG': 'R3206',
+    'SWMI': 'R3207',
+    'SWPR': 'R3208',
+    'SWRE': 'R3209',
+    'SWSV': 'R3210',
+    'SWWB': 'R3211',
+    'SWWH': 'R3212',
+    'SWWP': 'R3213'
+}
