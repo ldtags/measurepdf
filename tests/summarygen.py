89da83727732edea381c60be170e6295efd7f88b
Liam <ltags230@gmail.com>
2024-06-27 09:30:55 -0500

    more tests

--- tests/summarygen.py
+++ tests/summarygen.py
@@ -25,10 +25,25 @@ def build_summary(measure_ids: list[str]):
 
 
 MEASURES = [
-    'SWFS006-03', # embedded value table with no cids and weird text overlapping
-    'SWFS001-03', # embedded value table with cids
-    'SWHC045-03', # static value table
-    'SWWH025-07', # medium-sized subscript, images and list
+    'SWFS006-03',
+    # embedded value table with no cids
+
+    'SWFS001-03',
+    # embedded value table with cids
+
+    'SWHC045-03',
+    # static value table with no spanning
+
+    'SWWH025-07',
+    # medium-sized subscript
+    # unscaled images
+    # list
+
+    'SWFS017-03',
+    # static value table with column spans and row spans
+    # single digit superscript
+    # image rescaling
+    # list
 ]
 
 
