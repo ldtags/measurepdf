a11e070375d3e9ce0253dd936b408f91e1b985d1
Liam Tangney <ltags230@gmail.com>
2025-01-28 14:45:23 -0600

    implemented json retrieval method

--- src/resources/__init__.py
+++ src/resources/__init__.py
@@ -1,9 +1,23 @@
 import os
+import json
+from typing import Any
+
 
 _PATH = os.path.abspath(os.path.dirname(__file__))
 
-def get_path(file_name: str) -> str:
+
+def get_path(file_name: str, exists: bool = True) -> str:
     file_path = os.path.join(_PATH, file_name)
-    if not os.path.exists(file_path):
-        raise FileNotFoundError(f'no resource named {file_name} exists')
+    if exists and not os.path.exists(file_path):
+        raise FileNotFoundError(f"No resource named {file_name} exists")
+
     return file_path
+
+
+def get_json(file_name: str) -> dict[str, Any]:
+    _, ext = os.path.splitext(file_name)
+    if ext != ".json":
+        raise RuntimeError(f"File {file_name} must be a JSON file")
+
+    file_path = get_path(file_name)
+    return json.load(file_path)
