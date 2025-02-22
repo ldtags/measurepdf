8bea10bffefb473ed02a3b24ef6599b9e8740c24
Liam Tangney <ltags230@gmail.com>
2025-02-22 13:24:45 -0600

    added asset init file

---
+++ src/assets/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+
+_PATH = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_path(asset_path: str, exists: bool = True) -> str:
+    if asset_path.startswith("assets") and len(asset_path) >= 7:
+        asset_path = asset_path[6:]
+
+    file_path = os.path.join(_PATH, asset_path)
+    if exists and not os.path.exists(file_path):
+        raise FileNotFoundError(f"No asset exists at {asset_path}")
+
+    return file_path
