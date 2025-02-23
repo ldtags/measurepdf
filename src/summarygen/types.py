6ed0db6a76f1d5fd9233d240384a6d2163ee0b6b
Liam Tangney <ltags230@gmail.com>
2025-02-22 21:13:23 -0600

    formatting

--- src/summarygen/types.py
+++ src/summarygen/types.py
@@ -1,7 +1,7 @@
 from typing import Literal
 
 
-_TABLE_SPAN = tuple[tuple[int, int], tuple[int, int]]
+_TableSpan = tuple[tuple[int, int], tuple[int, int]]
 """Represents a row/col span in a table
 
 The first tuple contains the (y, x) coords of the span
@@ -10,7 +10,7 @@ The second tuple contains the (row, col) span sizes
 """
 
 
-_TABLE_ORIENT = Literal['top', 'left', 'top-left']
+_TableOrient = Literal["top", "left", "top-left"]
 """Represents the orientation of the table.
 
 This determines whether the header of the table is on either
