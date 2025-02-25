1871629ae6b29c9fd9054267c8d9f1261dfa3f66
Liam Tangney <ltags230@gmail.com>
2025-02-25 16:06:05 -0600

    documention

--- src/summarygen/models/enums.py
+++ src/summarygen/models/enums.py
@@ -2,6 +2,11 @@ from enum import Enum
 
 
 class ElementType(Enum):
+    """Defines several types of paragraph elements.
+
+    Use this enum to specify elements that require custom styling and display.
+    """
+
     Text = "Text"
     Space = "Space"
     Newline = "Newline"
@@ -10,6 +15,11 @@ class ElementType(Enum):
 
 
 class TextStyle(Enum):
+    """Defines several types of text styles.
+
+    Use this enum to add styling attributes to paragraph elements.
+    """
+
     Normal = "Normal"
     Strong = "Strong"
     Italic = "Italic"
