884148cd25505a44711d39d39e852e43b55de904
Liam Tangney <ltags230@gmail.com>
2025-03-05 11:25:51 -0600

    linting

--- tests/summarygen/builder.py
+++ tests/summarygen/builder.py
@@ -231,7 +231,7 @@ def parse_args() -> ap.Namespace:
     return parser.parse_args()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parse_args()
     measures = getattr(args, "measures", [])
     use_categories = getattr(args, "use_categories", [])
