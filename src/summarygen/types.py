d0f867d73b418ef590f37f7f370bc5679d4cf85d
Liam Tangney <ltags230@gmail.com>
2025-03-12 11:50:37 -0500

    removed unused type

--- src/summarygen/types.py
+++ src/summarygen/types.py
@@ -1,18 +1,12 @@
-from typing import Literal
+__all__ = [
+    "_TableSpan"
+]
 
 
 _TableSpan = tuple[tuple[int, int], tuple[int, int]]
-"""Represents a row/col span in a table
+"""Represents a row/col span in a table.
 
-The first tuple contains the (y, x) coords of the span
+The first tuple contains the (y, x) coords of the span.
 
-The second tuple contains the (row, col) span sizes
-"""
-
-
-_TableOrient = Literal["top", "left", "top-left"]
-"""Represents the orientation of the table.
-
-This determines whether the header of the table is on either
-the `top`, `left`, or both `top` and `left` sides of the table.
+The second tuple contains the (row, col) span sizes.
 """
